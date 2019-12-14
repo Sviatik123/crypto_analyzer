909bdfd492b67f632bc54e886da5b83b7b01bc42
maxSlkk <max.slushch@gmail.com>
2019-12-14 19:47:05 +0200

    add WORKING horizontal scrollbar

--- tkinter_test.py
+++ tkinter_test.py
@@ -27,7 +27,7 @@ def fill_table(required_columns=['symbol', 'bid', 'bid_size', 'ask', 'ask_size',
     table_treeview["columns"] = required_columns
     table_treeview["show"] = "headings"
     for col in required_columns:
-        table_treeview.column(col, width=90)
+        table_treeview.column(col, stretch=False, width=90)
         table_treeview.heading(col, text=col)
     tuples = result.fetchall()
     index = 0
@@ -37,7 +37,7 @@ def fill_table(required_columns=['symbol', 'bid', 'bid_size', 'ask', 'ask_size',
 
 
 def form_column_list():
-    col_list = []
+    col_list = ['symbol']
     '''form col list'''
     fill_table(col_list)
 
@@ -58,11 +58,14 @@ def get_pairs():
     return pairs
 
 
+#preparing root
 root = tk.Tk()
-root.minsize(1000, 500)
+root.minsize(1156, 500)
 root.title('Crypto Analyzer')
 root.resizable(False, False)
 
+
+#system menus
 menubar = tk.Menu(root)
 main_menu = tk.Menu(menubar, tearoff=0)
 main_menu.add_command(label="Refresh Database", command=refresh_database)
@@ -77,28 +80,26 @@ menubar.add_cascade(label="Help", menu=helpmenu)
 root.config(menu=menubar)
 
 
-table_frame = tk.LabelFrame(root, text='Frame1', padx=10, pady=10)
+#creating controls
+content = ttk.Label(root)
 
-table_frame.grid(row=0, column=0, padx=10)
-
-
-table_treeview = ttk.Treeview(table_frame, height=19)
+table_treeview = ttk.Treeview(content, height=19)
 fill_table()
-scbV = ttk.Scrollbar(table_frame, orient=tk.VERTICAL, command=table_treeview.yview)#not working
-table_treeview.configure(yscrollcommand=scbV.set)#not working
-table_treeview.pack(pady=5)
+treeXScroll = ttk.Scrollbar(content, orient=tk.HORIZONTAL)
+treeXScroll.configure(command=table_treeview.xview)
+table_treeview.configure(xscrollcommand=treeXScroll.set)
 
-refresh_btn = tk.Button(table_frame, text='Refresh', width=10)
-refresh_btn.pack(pady=5)
+refresh_btn = tk.Button(content, text='Refresh', width=10)
+pairs_combobox = ttk.Combobox(content, values=get_pairs())
+form_btn = tk.Button(content, text='Form Table', width=10, command=form_column_list)
 
 
-controls_frame = tk.LabelFrame(root, text='Search Options', padx=10, pady=10)
-controls_frame.grid(row=0, column=1, padx=10)
-
-pairs_combobox = ttk.Combobox(controls_frame, values=get_pairs())
-pairs_combobox.pack()
-
-form_btn = tk.Button(controls_frame, text='Form Table', width=10, command=form_column_list)
-form_btn.pack(pady=5)
+#placing controls
+content.pack(side=tk.TOP)
+table_treeview.grid(row=0, column=0, rowspan=6, columnspan=6, padx=(10, 5), pady=(10, 0))
+treeXScroll.grid(row=7, column=0, columnspan=6, sticky=tk.W + tk.E, padx=(10, 5), pady=(0, 10))
+refresh_btn.grid(row=8, column=0, pady=(10, 10))
+pairs_combobox.grid(row=0, column=7, padx=(5, 10), pady=(10, 10))
+form_btn.grid(row=8, column=7, sticky=tk.W, padx=(5, 10))
 
 root.mainloop()
