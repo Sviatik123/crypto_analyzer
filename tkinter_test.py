08194c722bf0f4233ced4d6c75c9c5a71597d28b
Sviatik <sviatoslavgamar2001@gmail.com>
2019-12-17 11:15:57 +0200

    fix grid

--- tkinter_test.py
+++ tkinter_test.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from tkinter import messagebox
 import tkinter.ttk as ttk
 import sqlalchemy as db
+
 # from database_filler import DatabaseFiller
 
 engine = db.create_engine("sqlite:///crypto_analyzer.db")
 connection = engine.connect()
 metadata = db.MetaData()
+
+
 # dbf = DatabaseFiller()
 
 
@@ -21,6 +24,8 @@ def generate_request(columns, db_table):
     request += 'FROM ' + db_table
     if currencies_combobox.get() != 'All':
         request += " WHERE symbol='" + currencies_combobox.get()[1:] + "'"
+    if order_combobox.get() in form_column_list():
+        request += " ORDER BY " + order_combobox.get()
     return request
 
 
@@ -32,7 +37,7 @@ def fill_table():
     result = engine.execute(request)
     table_treeview["columns"] = required_columns
     for col in required_columns:
-        table_treeview.column(col, stretch=False, width=64)
+        table_treeview.column(col, stretch=False, width=68)
         table_treeview.heading(col, text=col)
     table_treeview["show"] = "headings"
     tuples = result.fetchall()
@@ -126,7 +131,6 @@ root.minsize(1360, 570)
 root.title('Crypto Analyzer')
 root.resizable(False, False)
 
-
 # system menus
 menubar = tk.Menu(root)
 main_menu = tk.Menu(menubar, tearoff=0)
@@ -140,7 +144,6 @@ helpmenu.add_command(label="About", command=print_info)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
-
 
 # creating controls
 content = tk.Frame(root)
@@ -159,6 +162,8 @@ currencies_combobox.current(0)
 form_btn = tk.Button(instrument, text='Form Table', width=10, command=fill_table)
 label_checkboxes = tk.Label(instrument, text='Select columns:')
 label_radio = tk.Label(instrument, text='Select tickers:')
+label_currencies = tk.Label(instrument, text='Select currency: ')
+label_order = tk.Label(instrument, text='Order by: ')
 
 # checkboxes and radiobuttons
 check1 = tk.StringVar()
@@ -181,7 +186,8 @@ check_btn2 = tk.Checkbutton(instrument, text='bid_size', variable=check2, onvalu
 check_btn3 = tk.Checkbutton(instrument, text='ask', variable=check3, onvalue='ask', offvalue='')
 check_btn4 = tk.Checkbutton(instrument, text='ask_size', variable=check4, onvalue='ask_size', offvalue='')
 check_btn5 = tk.Checkbutton(instrument, text='daily_change', variable=check5, onvalue='daily_change', offvalue='')
-check_btn6 = tk.Checkbutton(instrument, text='daily_change_relative', variable=check6, onvalue='daily_change_relative', offvalue='')
+check_btn6 = tk.Checkbutton(instrument, text='daily_change_relative', variable=check6, onvalue='daily_change_relative',
+                            offvalue='')
 check_btn7 = tk.Checkbutton(instrument, text='last_price', variable=check7, onvalue='last_price', offvalue='')
 check_btn8 = tk.Checkbutton(instrument, text='volume', variable=check8, onvalue='volume', offvalue='')
 check_btn9 = tk.Checkbutton(instrument, text='high', variable=check9, onvalue='high', offvalue='')
@@ -189,31 +195,39 @@ check_btn10 = tk.Checkbutton(instrument, text='low', variable=check10, onvalue='
 check_btn11 = tk.Checkbutton(instrument, text='frr', variable=check11, onvalue='frr', offvalue='')
 check_btn12 = tk.Checkbutton(instrument, text='bid_period', variable=check12, onvalue='bid_period', offvalue='')
 check_btn13 = tk.Checkbutton(instrument, text='ask_period', variable=check13, onvalue='ask_period', offvalue='')
-check_btn14 = tk.Checkbutton(instrument, text='frr_amount_available', variable=check14, onvalue='frr_amount_available', offvalue='')
+check_btn14 = tk.Checkbutton(instrument, text='frr_amount_available', variable=check14, onvalue='frr_amount_available',
+                             offvalue='')
 
-check_btn1.grid(row=2, column=0, sticky=tk.W)
-check_btn2.grid(row=2, column=1, sticky=tk.W, padx=(0, 10))
-check_btn3.grid(row=3, column=0, sticky=tk.W)
-check_btn4.grid(row=3, column=1, sticky=tk.W, padx=(0, 10))
-check_btn5.grid(row=4, column=0, sticky=tk.W)
-check_btn6.grid(row=4, column=1, sticky=tk.W, padx=(0, 10))
-check_btn7.grid(row=5, column=0, sticky=tk.W)
-check_btn8.grid(row=5, column=1, sticky=tk.W, padx=(0, 10))
-check_btn9.grid(row=6, column=0, sticky=tk.W)
-check_btn10.grid(row=6, column=1, sticky=tk.W, padx=(0, 10))
-check_btn11.grid(row=7, column=0, sticky=tk.W)
-check_btn12.grid(row=7, column=1, sticky=tk.W, padx=(0, 10))
-check_btn13.grid(row=8, column=0, sticky=tk.W)
-check_btn14.grid(row=8, column=1, sticky=tk.W, padx=(0, 10))
+columns = ['symbol', 'bid', 'bid_size', 'ask', 'ask_size', 'daily_change', 'daily_change_relative', 'last_price',
+           'volume', 'high', 'low', 'frr', 'bid_period', 'ask_period', 'frr_amount_available']
+
+order_combobox = ttk.Combobox(instrument, values=columns)
+order_combobox.current(0)
+
+check_btn1.grid(row=3, column=0, sticky=tk.W)
+check_btn2.grid(row=3, column=1, sticky=tk.W, padx=(0, 10))
+check_btn3.grid(row=4, column=0, sticky=tk.W)
+check_btn4.grid(row=4, column=1, sticky=tk.W, padx=(0, 10))
+check_btn5.grid(row=5, column=0, sticky=tk.W)
+check_btn6.grid(row=5, column=1, sticky=tk.W, padx=(0, 10))
+check_btn7.grid(row=6, column=0, sticky=tk.W)
+check_btn8.grid(row=6, column=1, sticky=tk.W, padx=(0, 10))
+check_btn9.grid(row=7, column=0, sticky=tk.W)
+check_btn10.grid(row=7, column=1, sticky=tk.W, padx=(0, 10))
+check_btn11.grid(row=8, column=0, sticky=tk.W)
+check_btn12.grid(row=8, column=1, sticky=tk.W, padx=(0, 10))
+check_btn13.grid(row=9, column=0, sticky=tk.W)
+check_btn14.grid(row=9, column=1, sticky=tk.W, padx=(0, 10))
 
 radio = tk.StringVar()
 
 radio_btn1 = tk.Radiobutton(instrument, text='Pair', variable=radio, value='tickers', command=change_checkboxes)
-radio_btn2 = tk.Radiobutton(instrument, text='Currency', variable=radio, value='tickers_on_currency', command=change_checkboxes)
+radio_btn2 = tk.Radiobutton(instrument, text='Currency', variable=radio, value='tickers_on_currency',
+                            command=change_checkboxes)
 radio_btn1.select()
 radio_btn2.deselect()
-radio_btn1.grid(row=10, column=0, sticky=tk.W)
-radio_btn2.grid(row=10, column=1, sticky=tk.W)
+radio_btn1.grid(row=11, column=0, sticky=tk.W)
+radio_btn2.grid(row=11, column=1, sticky=tk.W)
 
 change_checkboxes()
 
@@ -224,10 +238,13 @@ treeXScroll.grid(row=7, column=0, columnspan=6, sticky=tk.W + tk.E, padx=(10, 5)
 treeYScroll.grid(row=0, column=7, rowspan=6, sticky=tk.N + tk.S, padx=(0, 5), pady=(10, 0))
 
 instrument.grid(row=0, column=1, sticky=tk.NE)
-currencies_combobox.grid(row=0, column=0, padx=(5, 10), pady=(10, 10))
-form_btn.grid(row=11, column=1, sticky=tk.E, padx=(5, 10), pady=(10, 0))
-label_checkboxes.grid(row=1, column=0, sticky=tk.W, pady=(10, 0))
-label_radio.grid(row=9, column=0, sticky=tk.W, pady=(10, 0))
+label_currencies.grid(row=0, column=0, padx=(0, 0), pady=(10, 10))
+currencies_combobox.grid(row=0, column=1, padx=(0, 10), pady=(10, 10))
+label_order.grid(row=1, column=0, padx=(0, 0), pady=(10, 10), sticky=tk.W)
+order_combobox.grid(row=1, column=1, padx=(0, 10), pady=(10, 10))
+form_btn.grid(row=12, column=1, sticky=tk.E, padx=(5, 10), pady=(10, 0))
+label_checkboxes.grid(row=2, column=0, sticky=tk.W, pady=(10, 0))
+label_radio.grid(row=10, column=0, sticky=tk.W, pady=(10, 0))
 
 fill_table()
 
